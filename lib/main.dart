4dca5678a55852e84f123d15e1370cf71a91eb4d
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-12-24 03:42:02 +0700

    feat: lock landscape view device

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:suitmedia_application_test/src/app.dart';
 
 /// Main Function
-/// 
+///
 /// The entry point of the application.
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(
     const App(),
   );
